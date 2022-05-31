e0f7441b49468f005812a26586d7be1543bd7287
sena <180290612@firat.edu.tr>
2022-05-31 14:04:36 +0300

    Create Form5.cs

---
+++ Form5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ff
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 main = new Form3();
+            main.Show();
+
+        }
+    }
+}
