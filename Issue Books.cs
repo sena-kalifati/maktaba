07aec7777fcd6ea87f4c09415d9edd3e8501b399
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-01 22:47:35 +0300

    Create Issue Books.cs

---
+++ Issue Books.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ff
+{
+    public partial class Issue_Books : Form
+    {
+        public Issue_Books()
+        {
+            InitializeComponent();
+        }
+
+        private void Issue_Books_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 main = new Form3();
+            main.Show();
+
+        }
+    }
+}
