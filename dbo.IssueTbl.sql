e1be6f767ce569b29e5b43e165e41a2adc8d0d3e
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-03 00:10:09 +0300

    Create dbo.IssueTbl.sql

---
+++ dbo.IssueTbl.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[IssueTbl] (
+    [IssueNum]   INT           NOT NULL,
+    [StdId]      INT           NOT NULL,
+    [StdName]    VARCHAR (50)  NOT NULL,
+    [StdDept]    INT           NOT NULL,
+    [StdPhone]   VARCHAR (50)  NOT NULL,
+    [BookIssued] VARCHAR (100) NOT NULL,
+    [IssueDate]  DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([IssueNum] ASC)
+);
+
