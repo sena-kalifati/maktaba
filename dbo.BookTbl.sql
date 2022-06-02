1fd5e5dcf66d26ec94ec22d70d672394d81febb6
sena <180290612@firat.edu.tr>
2022-06-03 00:12:19 +0300

    Create dbo.BookTbl.sql

---
+++ dbo.BookTbl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[BookTbl] (
+    [BookName]  VARCHAR (50) NOT NULL,
+    [Author]    VARCHAR (50) NOT NULL,
+    [Publisher] VARCHAR (50) NOT NULL,
+    [Price]     INT          NOT NULL,
+    [Qty]       INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([BookName] ASC)
+);
+
