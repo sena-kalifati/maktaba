3a859ea79f7cdd5033373b32e9e33d330e30cf2d
sena <180290612@firat.edu.tr>
2022-06-02 23:55:56 +0300

    Create dbo.StudentTbl.sql

---
+++ dbo.StudentTbl.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[StudentTbl] (
+    [BookName]  INT          NOT NULL,
+    [Author]    VARCHAR (50) NOT NULL,
+    [Publisher] VARCHAR (50) NOT NULL,
+    [Price]     INT          NOT NULL,
+    [Qty]       VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([BookName] ASC)
+);
+
