969babfb1137fd209c190aac1e3a6ccec9b1b8b5
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-02 23:59:21 +0300

    Create dbo.LibrarianTbl.sql

---
+++ dbo.LibrarianTbl.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[LibrarianTbl] (
+    [LibId]        INT           NOT NULL,
+    [LibName]      VARCHAR (50)  NOT NULL,
+    [LibePassword] NVARCHAR (50) NOT NULL,
+    [LipePhone]    VARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([LibId] ASC)
+);
+
