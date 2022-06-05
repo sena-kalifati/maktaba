07b465d3fd5573cf8647e41464761d6ca8019a45
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-05 18:17:31 +0300

    Update Form4.cs

--- Form4.cs
+++ Form4.cs
@@ -5,11 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace ff
 {
     public partial class Form4 : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-8NJIN7L;Initial Catalog=maktaba;Integrated Security=True");
         public Form4()
         {
             InitializeComponent();
@@ -46,6 +47,46 @@ namespace ff
             Form3 main = new Form3();
             main.Show();
 
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlCommand cmd = new SqlCommand("insert into StudentTbl values(" + StdId.Text + ",'" + StdName.Text + "','" + StdDep.Text + "','" + StdSem.Text + "','" + StdPhone.Text + "')", Con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("librarian added successfully");
+            Con.Close();
+         
+        }
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+          
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
         }
     }
 }
