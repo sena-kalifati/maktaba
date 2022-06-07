f423fd82243cae447ab86829aa7b4cb5fbed71d7
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-07 23:51:21 +0300

    Update Issue Books.cs

--- Issue Books.cs
+++ Issue Books.cs
@@ -5,11 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace ff
 {
     public partial class Issue_Books : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-8NJIN7L;Initial Catalog=maktaba;Integrated Security=True");
         public Issue_Books()
         {
             InitializeComponent();
@@ -26,6 +27,40 @@ namespace ff
             Form3 main = new Form3();
             main.Show();
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlCommand cmd = new SqlCommand("insert into IssueTbl values(" + IssueNum.Text + ",'" + StdId.Text + "','" + StdName.Text + "','" + StdDept.Text + "','" + StdPhone.Text + "','" + BookIssued.Text + "','" + IssueDate.Text + "')", Con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Issue added successfully");
+            Con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       
+
+        }
+       
+
+        private void IssueDGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            IssueNum.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            StdId.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            StdName.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            StdDept.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            StdPhone.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            BookIssued.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+            IssueDate.Text = IssueDGV.SelectedRows[0].Cells[0].Value.ToString();
+
+        
         }
     }
 }
