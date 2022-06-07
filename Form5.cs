967c1fbfd0c18b6d31df7477757d929936a546b5
sena <180290612@firat.edu.tr>
2022-06-07 23:56:12 +0300

    Update Form5.cs

--- Form5.cs
+++ Form5.cs
@@ -5,21 +5,63 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ff
 {
     public partial class Form5 : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-8NJIN7L;Initial Catalog=maktaba;Integrated Security=True");
         public Form5()
         {
             InitializeComponent();
         }
-
+        
         private void button4_Click(object sender, EventArgs e)
         {
             this.Hide();
             Form3 main = new Form3();
             main.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlCommand cmd = new SqlCommand("insert into BookTbl values(" + BookName.Text + ",'" + Author.Text + "','" + Publisher.Text + "','" + Price.Text + "','" + Qty.Text + "')", Con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("librarian added successfully");
+            Con.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+       
+
+        }
+       
+        private void maskedTextBox4_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
 
         }
     }
