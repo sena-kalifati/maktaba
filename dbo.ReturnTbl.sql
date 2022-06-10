7a706fe43c42a9d27fc63cc03922ae5a824709e8
sena <180290612@firat.edu.tr>
2022-06-10 23:23:33 +0300

    Create dbo.ReturnTbl.sql

---
+++ dbo.ReturnTbl.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ReturnTbl] (
+    [ReturnNum]    INT           NOT NULL,
+    [StdId]        INT           NOT NULL,
+    [StdName]      VARCHAR (50)  NOT NULL,
+    [StdDept]      VARCHAR (50)  NOT NULL,
+    [StdPhone]     VARCHAR (50)  NOT NULL,
+    [Bookreturned] VARCHAR (100) NOT NULL,
+    [IssueDate]    DATE          NOT NULL,
+    [ReturnDaate]  DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([ReturnNum] ASC)
+);
+
