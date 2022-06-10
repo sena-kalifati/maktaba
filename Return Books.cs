7d781e16de23f32a4f10ffcc859835e9301e6d34
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-10 23:50:00 +0300

    Update Return Books.cs

--- Return Books.cs
+++ Return Books.cs
@@ -5,11 +5,13 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ff
 {
     public partial class Return_Books : Form
     {
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-8NJIN7L;Initial Catalog=maktaba;Integrated Security=True");
         public Return_Books()
         {
             InitializeComponent();
@@ -22,5 +24,21 @@ namespace ff
             main.Show();
 
         }
+
+        private void Return_Books_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Con.Open();
+            SqlCommand cmd = new SqlCommand("insert into StudentTbl values()");
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("librarian added successfully");
+            Con.Close();
+           
+        }
+    }
     }
 }
