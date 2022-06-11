1e2c22fd017bce26e96ab20878db2814c34015d4
sena <105065697+elakalifati@users.noreply.github.com>
2022-06-11 23:55:53 +0300

    Update Return Books.cs

--- Return Books.cs
+++ Return Books.cs
@@ -32,13 +32,37 @@ namespace ff
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             Con.Open();
-            SqlCommand cmd = new SqlCommand("insert into StudentTbl values()");
+            SqlCommand cmd = new SqlCommand("insert into ReturnTbl values(" + ReturnNum.Text + ",'" + StdId.Text + "','" + StdName.Text + "','" + StdDept.Text + "','" + StdPhone.Text + "','" + Bookreturned.Text + IssueDate + "','" + ReturnDaate + "')", Con);
             cmd.ExecuteNonQuery();
-            MessageBox.Show("librarian added successfully");
+            MessageBox.Show("Issue added successfully");
             Con.Close();
-           
+
+        }
+        public void populate()
+        {
+            Con.Open();
+            string query = "select * from StudentTbl";
+            SqlDataAdapter da = new SqlDataAdapter(query, Con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);
+            var ds = new DataSet();
+            da.Fill(ds);
+            returngve.DataSource = ds.Tables[0];
+
+            Con.Close();
+        }
+
+
+        private void maskedTextBox2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
     }
-}
+
