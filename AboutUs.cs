33df2a5734afc64f26629abfadc00abf7fa5bcbf
sena <180290612@firat.edu.tr>
2022-06-11 23:57:06 +0300

    Create AboutUs.cs

---
+++ AboutUs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ff
+{
+    public partial class AboutUs : Form
+    {
+        public AboutUs()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutUs_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
